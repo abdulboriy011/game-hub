f82c7ba0d5e95f89843168f83c4dcfd1eed15017
abdulboriy011 <abdulhajsukurov1@gmail.com>
2024-05-30 09:36:06 +0300

    Create responsive layout and add colorModeSwitch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { Grid, GridItem, Show} from "@chakra-ui/react"
 import './App.css'
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
-    <>
-     
-    </>
+    <Grid templateAreas={{
+      base: `"nav" "main"`,
+      lg: `"nav nav" "aside main"`,
+    }}>
+      <GridItem  area={"nav"}><Navbar/></GridItem>
+    <Show above="lg">
+    <GridItem bg={"royalblue"} area={"aside"} >Aside</GridItem>
+    </Show>
+      <GridItem bg={"red"} area={"main"} >Main</GridItem>
+    </Grid>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import { HStack, Image } from "@chakra-ui/react"
+import logo from "../assets/logo.webp"
+import ColorModeSwitch from "./ColorModeSwitch"
+const Navbar = () => {
+  return (
+  <HStack justifyContent={"space-between"} paddingX={"10px"}>
+    <Image src={logo} boxSize={"32px"}/>
+    <ColorModeSwitch />
+  </HStack>
+  )
+}
+
+export default Navbar
